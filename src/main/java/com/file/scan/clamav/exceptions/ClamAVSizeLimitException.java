53082814d4cb67d75e9be95cc92874c8a9e140ae
stuntioflr <40563977+stuntioflr@users.noreply.github.com>
2021-06-18 12:46:28 +0530

    deleted

--- src/main/java/com/file/scan/clamav/exceptions/ClamAVSizeLimitException.java
+++
@@ -1,8 +0,0 @@
-package com.file.scan.clamav.exceptions;
-
-public class ClamAVSizeLimitException extends  RuntimeException{
-
-    public ClamAVSizeLimitException(String msg) {
-        super(msg);
-    }
-}
